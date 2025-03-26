64c53ea12d95c3e9d918dd5ac97555097942612d
NITISH SATI <nitishsati8@gmail.com>
2025-03-26 18:12:42 +0530

    ok

--- src/main/java/com/nagp/products/config/WebConfig.java
+++ src/main/java/com/nagp/products/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://13.235.208.227","http://localhost:3000","http://localhost:8080","http://13.235.208.227:8089")
+                        .allowedOrigins("https://13.235.208.227","http://localhost:3000","http://localhost:8080","https://13.235.208.227:8089")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
