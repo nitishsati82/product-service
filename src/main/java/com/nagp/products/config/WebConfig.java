6e35321970c18236b34bd11aab257b9f135d0bb3
NITISH SATI <nitishsati8@gmail.com>
2025-03-26 13:19:21 +0530

    ok

--- src/main/java/com/nagp/products/config/WebConfig.java
+++ src/main/java/com/nagp/products/config/WebConfig.java
@@ -18,6 +18,7 @@ public class WebConfig {
         corsConfiguration.addAllowedOrigin("http://localhost:3001");
         corsConfiguration.addAllowedOrigin("http://localhost:8080");
         corsConfiguration.addAllowedOrigin("http://13.235.208.227:8089");
+        corsConfiguration.addAllowedOrigin("https://13.235.208.227:8089");
         corsConfiguration.addAllowedHeader("*"); // Allow all headers
         corsConfiguration.addAllowedMethod("*"); // Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
         corsConfiguration.setAllowCredentials(true); // Allow cookies if needed
